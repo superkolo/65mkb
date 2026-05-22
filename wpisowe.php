abe4c29a68ffad43bb774d1a05f0131972c0a793
Michał Górski <michalmarcingorski@gmail.com>
2026-05-22 16:20:58 +0200

    Update entry fees page and 2026 schedule dates

--- wpisowe.php
+++ wpisowe.php
@@ -3,7 +3,69 @@ $title = "Wpisowe";
 include 'header.php';
 ?>
 
-<h1>Wpisowe</h1>
-<p>W przygotowaniu...</p>
+<section class="entry-fees">
+  <h2>Wpisowe (od osoby)</h2>
+  <p class="entry-fees__lead">cennik dla rejestracji online<br>płatność na miejscu +10 zł</p>
+
+  <div class="entry-fees__table-wrap">
+    <table class="entry-fees__table">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Członek PZBS</th>
+          <th>Juniorzy<sup>1</sup></th>
+          <th>Uczniowie<br>do 19 lat<sup>1</sup></th>
+          <th>Niezrzeszeni<sup>2</sup></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><span class="fee-dot fee-dot--empty"></span>Grand Prix Polski Par</td>
+          <td>125 zł</td>
+          <td>70 zł</td>
+          <td>35 zł</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td><span class="fee-dot fee-dot--red"></span>MPT - eliminacje<sup>3</sup></td>
+          <td>160 zł</td>
+          <td>100 zł</td>
+          <td>60 zł</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td><span class="fee-dot fee-dot--blue"></span>Turniej Kongresowy (3x10)</td>
+          <td>75 zł</td>
+          <td>50 zł</td>
+          <td>20 zł</td>
+          <td>95 zł</td>
+        </tr>
+        <tr>
+          <td><span class="fee-dot fee-dot--blue"></span>Turniej Kongresowy (4x10)</td>
+          <td>95 zł</td>
+          <td>60 zł</td>
+          <td>25 zł</td>
+          <td>115 zł</td>
+        </tr>
+        <tr>
+          <td><span class="fee-dot fee-dot--yellow"></span>Turniej Amatorów<sup>4</sup></td>
+          <td>10 zł</td>
+          <td>10 zł</td>
+          <td>10 zł</td>
+          <td>10 zł</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <ol class="entry-fees__notes">
+    <li>Dla osób z opłaconą bieżącą składką PZBS oraz młodzieży zagranicznej. Nie dotyczy turniejów amatorskich.</li>
+    <li>W turniejach z cyklu GPPP oraz w MPT prawo startu mają wyłącznie zawodnicy z opłaconą bieżącą składką PZBS oraz zawodnicy zagraniczni z opłaconą składką w zagranicznej federacji.</li>
+    <li>Za każdą kolejną fazę 50 zł niezależnie od kategorii.</li>
+    <li>Uprawnieni do udziału w turnieju są amatorzy, tzn. gracze niezrzeszeni w PZBS lub zrzeszeni w PZBS o WK nie przekraczającym 1.5.</li>
+    <li>Nagrody będą wypłacane przelewem.</li>
+    <li>Zawodnicy z Rosji i Białorusi niezrzeszeni w PZBS nie zostaną dopuszczeni do gry. Pozostali zawodnicy zagraniczni płacą wpisowe jak członkowie PZBS, z tym że owi zawodnicy muszą być zarejestrowani w swoim macierzystym związku brydżowym.</li>
+  </ol>
+</section>
 
 <?php include 'footer.php'; ?>
