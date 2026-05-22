857eb0de67c0a0587c26978830643000ed502b73
Michał Górski <michalmarcingorski@gmail.com>
2026-05-22 16:44:13 +0200

    Add GPPT 2026 regulations

--- gppt.php
+++ gppt.php
@@ -1,10 +1,97 @@
-<!-- ================= PRZYKŁADOWA PODSTRONA ================= -->
 <?php
-$title = "Wpisowe";
+$title = "Regulamin GPPT";
 include 'header.php';
 ?>
 
-<h1>Regulamin turnieju teamów</h1>
-<p>W przygotowaniu...</p>
+<section class="rules team-rules">
+  <h2>Komunikat organizacyjny BUDIMEX Grand Prix Polski Teamów 2026</h2>
+  <p class="rules__intro">Turniej rozgrywany jest w ramach 65. Międzynarodowego Kongresu Bałtyckiego w Sopocie.</p>
+
+  <section class="rules__section">
+    <h3>I. Informacje ogólne</h3>
+    <ol>
+      <li>Organizatorem turnieju BUDIMEX Grand Prix Polski Teamów 2026 w Sopocie jest Pomorski Wojewódzki Związek Brydża Sportowego.</li>
+      <li>Turniej rozgrywany jest w dniach 27-29.07.2026 w Hali Stulecia w Sopocie przy ul. Goyki 7.</li>
+      <li>Zgłoszenia przyjmowane są wraz z płatnością poprzez system rejestracji.</li>
+      <li>W turnieju obowiązują przepisy PZBS oraz <a target="_blank" href="https://www.pzbs.pl/regulaminy-stale/3540-regulamin-grand-prix-polski-teamow">Regulamin Grand Prix Polski Teamów PZBS</a>.</li>
+      <li>Harmonogram, wysokość wpisowego oraz link do rejestracji są dostępne na stronie Kongresu.</li>
+      <li>W turnieju mogą brać udział teamy 4-6 osobowe.</li>
+      <li>W szczególnych przypadkach Sędzia Główny może zdecydować o zmianie formatu rozgrywek w stopniu niezbędnym do sprawnego przeprowadzenia zawodów.</li>
+    </ol>
+  </section>
+
+  <section class="rules__section">
+    <h3>II. System rozgrywek</h3>
+    <ol>
+      <li>Turniej jest liczony na IMP z przeliczeniem na VP.</li>
+      <li>GPPT będzie rozgrywany w dwóch fazach:
+        <ul>
+          <li>eliminacje,</li>
+          <li>faza pucharowa.</li>
+        </ul>
+      </li>
+      <li>Eliminacje zostaną rozegrane systemem na dochodzenie bez powtórzeń.</li>
+      <li>Dokładna liczba rund i liczba rozdań w rundzie zostaną podane po zamknięciu zgłoszeń, z uwzględnieniem liczby teamów i wymogów regulaminu centralnego GPPT.</li>
+      <li>Do pierwszej rundy teamy zostaną rozstawione na podstawie Rankingu PZBS według sumy punktów rankingowych 4 najwyżej sklasyfikowanych zawodników.</li>
+      <li>Teamy od 1 do n/2 będą gospodarzami meczów, a teamy od n/2+1 do n gośćmi. W przypadku nieparzystej liczby drużyn w pierwszej rundzie pauzuje team środkowy lub inny wskazany ze względów organizacyjnych.</li>
+      <li>Drużyna pauzująca otrzymuje w danej rundzie 12 VP.</li>
+      <li>Do fazy pucharowej awansuje 8 drużyn.</li>
+    </ol>
+  </section>
+
+  <section class="rules__section">
+    <h3>III. Kolejność miejsc po eliminacjach</h3>
+    <p>W przypadku równej liczby VP o wyższym miejscu decydują kolejno:</p>
+    <ol>
+      <li>wyższe miejsce przed ostatnią rundą, a w drugiej kolejności przed rundą przedostatnią,</li>
+      <li>punktacja Buchholz, czyli suma wyników przeciwników,</li>
+      <li>większa różnica IMPów,</li>
+      <li>losowanie.</li>
+    </ol>
+  </section>
+
+  <section class="rules__section">
+    <h3>IV. Faza pucharowa</h3>
+    <ol>
+      <li>W fazie pucharowej rozgrywane są mecze 32-rozdaniowe, 4 x 8 rozdań.</li>
+      <li>Drużyny otrzymują numery 1-8 zgodnie z kolejnością po eliminacjach.</li>
+      <li>Wybór przeciwnika nastąpi bezpośrednio po upłynięciu okresu reklamacyjnego zakończonego etapu. W przypadku nieobecności przedstawiciela drużyny podczas wybierania, drużyna jest automatycznie parowana z drużyną o najwyższym niewybranym numerze.</li>
+      <li>W ćwierćfinale przeciwnika wybierają kolejno drużyny 1-3 spośród drużyn 4-8.</li>
+      <li>W półfinale przeciwnika wybiera drużyna 1 z pozostałych, z zastrzeżeniem że zwycięzcy meczu przejmują numer przeciwnika, o ile przeciwnik miał numer niższy.</li>
+      <li>Przegrani w ćwierćfinałach kończą rozgrywki na miejscach 5-8 zgodnie z kolejnością po eliminacjach.</li>
+      <li>Zwycięzcy półfinałów awansują do finału, a przegrani grają mecz o 3. miejsce.</li>
+      <li>Gospodarzem meczu jest drużyna o niższym numerze.</li>
+      <li>Gospodarz zajmuje miejsca jako drugi w segmentach 2 i 4.</li>
+      <li>W fazie pucharowej drużyna gospodarzy otrzymuje carry-over w wysokości odpowiadającej 10% liczby rozdań przewidzianych do rozegrania w danym meczu oraz dodatkowo 3, 2, 1 IMP dla pierwszych trzech drużyn po eliminacjach.</li>
+      <li>W przypadku przejęcia przez drużynę numeru przysługuje jej jedynie carry-over w wysokości 10% liczby rozdań przewidzianych do rozegrania w danym meczu, bez dodatkowego bonusu.</li>
+      <li>W przypadku zmniejszenia liczby rozdań w meczu play-off za zgodą Sędziego Głównego carry-over nie zmienia się.</li>
+    </ol>
+  </section>
+
+  <section class="rules__section">
+    <h3>V. Okres reklamacyjny i korekty zapisów</h3>
+    <ol>
+      <li>Okres reklamacyjny w myśl przepisu 79C1 MPB upływa:
+        <ul>
+          <li>w eliminacjach - 15 minut po rozpoczęciu następnego meczu, a w przypadku ostatniego meczu 15 minut po wpłynięciu ostatniego zapisu,</li>
+          <li>w meczach pucharowych - 15 minut po wpłynięciu ostatniego zapisu.</li>
+        </ul>
+      </li>
+      <li>Błąd w zapisie może być poprawiony po upływie okresu reklamacyjnego zgodnie z przepisem 79C2 MPB za zgodą Sędziego Głównego.</li>
+      <li>Taka zmiana zapisu nie może wpłynąć na wydane dotąd nagrody ani na rozpoczęte mecze w fazie pucharowej lub eliminacyjnej.</li>
+      <li>Sędzia Główny każdorazowo decyduje, czy taka zmiana może wpłynąć na nagrody jeszcze niewypłacone, klasyfikację długofalową Kongresu albo zwycięstwo w meczu pucharowym.</li>
+    </ol>
+  </section>
+
+  <section class="rules__section">
+    <h3>VI. Nagrody i punkty</h3>
+    <ol>
+      <li>Fundusz nagród turnieju stanowi co najmniej 50% wpisowego.</li>
+      <li>Nagrody finansowe wypłacane są kapitanowi drużyny.</li>
+      <li>Punkty długofalowe GPPT przyznawane są zgodnie z regulaminem centralnym PZBS.</li>
+      <li>Zawodnik otrzymuje punkty GPPT pod warunkiem rozegrania minimalnej liczby rozdań wymaganej regulaminem centralnym.</li>
+    </ol>
+  </section>
+</section>
 
 <?php include 'footer.php'; ?>
