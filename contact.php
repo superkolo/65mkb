5ab6c252fe3a95983878dbd03309346b71a1eccf
Michał Górski <michalmarcingorski@gmail.com>
2026-06-22 22:01:11 +0200

    Add organising committee and contact email

--- contact.php
+++ contact.php
@@ -5,6 +5,13 @@ include 'header.php';
 
 <h1>Kontakt</h1>
 <p>Pytania prosimy kierować na adres <a href="mailto:contact@sopotbridge.pl">contact@sopotbridge.pl</a>
-<br>Dyrektor Kongresu: <a target="_blank" href="https://msc.com.pl/cezar/?p=21&pid=4783">Michał Górski </p>
+<br>Dyrektor Kongresu: <a target="_blank" href="https://msc.com.pl/cezar/?p=21&pid=4783">Michał Górski</a>, <a href="mailto:michal.gorski@bridge.gda.pl">michal.gorski@bridge.gda.pl</a></p>
+
+<h2>Komitet organizacyjny</h2>
+<ul>
+  <li>Andrzej Twardowski</li>
+  <li>Robert Krawczyk</li>
+  <li>Marta Kondraciuk</li>
+</ul>
 
 <?php include 'footer.php'; ?>
--- en/contact.php
+++ en/contact.php
@@ -5,6 +5,13 @@ include __DIR__ . '/header.php';
 
 <h2>Contact</h2>
 <p>Please send questions to <a href="mailto:contact@sopotbridge.pl">contact@sopotbridge.pl</a><br>
-Congress Director: <a target="_blank" href="https://msc.com.pl/cezar/?p=21&pid=4783">Michał Górski</a></p>
+Congress Director: <a target="_blank" href="https://msc.com.pl/cezar/?p=21&pid=4783">Michał Górski</a>, <a href="mailto:michal.gorski@bridge.gda.pl">michal.gorski@bridge.gda.pl</a></p>
+
+<h2>Organising Committee</h2>
+<ul>
+  <li>Andrzej Twardowski</li>
+  <li>Robert Krawczyk</li>
+  <li>Marta Kondraciuk</li>
+</ul>
 
 <?php include __DIR__ . '/footer.php'; ?>
