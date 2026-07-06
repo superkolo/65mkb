aa7b8e961058dd53266ff33deaefad4f6f6a890c
Michał Górski <michalmarcingorski@gmail.com>
2026-07-06 09:37:54 +0200

    Update social image URLs for the congress site

--- en/header.php
+++ en/header.php
@@ -13,13 +13,13 @@ $polishHref = ($currentPage === 'index.php') ? '../' : '../' . $currentPage;
 <meta property="og:site_name" content="65th International Baltic Congress">
 <meta property="og:title" content="<?php echo htmlspecialchars($title, ENT_QUOTES); ?> - 65th International Baltic Congress">
 <meta property="og:description" content="Sopot, 22-30 July 2026. Hala Stulecia, ul. Goyki 7.">
-<meta property="og:image" content="https://sopotbridge.pl/og-image.png">
-<meta property="og:image:secure_url" content="https://sopotbridge.pl/og-image.png">
+<meta property="og:image" content="https://wyniki.pzbs.pl/2026/baltycki/og-image.png">
+<meta property="og:image:secure_url" content="https://wyniki.pzbs.pl/2026/baltycki/og-image.png">
 <meta property="og:image:type" content="image/png">
 <meta property="og:image:width" content="1200">
 <meta property="og:image:height" content="630">
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:image" content="https://sopotbridge.pl/og-image.png">
+<meta name="twitter:image" content="https://wyniki.pzbs.pl/2026/baltycki/og-image.png">
 <link rel="stylesheet" href="../style.css?t=<?php echo filemtime(__DIR__ . '/../style.css'); ?>">
 </head>
 <body>
